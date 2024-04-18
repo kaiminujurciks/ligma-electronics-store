c850d14609ca9c006950dbaaff6c2b476c66f0cb
mrkss02 <mrkss02@MRKSS02>
2024-04-18 16:39:52 +0300

    Add project files.

---
+++ ligma-electronics-store/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using ligma_electronics_store.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ligma_electronics_store.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
